f7a0bb73795106e0298ba85d292b16634d4d33d7
Chamith Udayanga <udayangaac@gmail.com>
2025-07-11 23:50:45 +0530

    Update the .rb file

--- Formula/multi-proxy.rb
+++ Formula/multi-proxy.rb
@@ -1,9 +1,9 @@
 class MultiProxy < Formula
   desc "A lightweight command-line tool to spin up multiple reverse proxies with logging and debugging support."
   homepage "https://github.com/udayangaac/multi-proxy"
-  url "https://github.com/udayangaac/multi-proxy/releases/download/v1.0.0/multi-proxy"
+  url "https://github.com/udayangaac/multi-proxy/releases/download/v1.0.2/multi-proxy"
   sha256 "d3438dbc7f3ccd1516fc7ab35f8a35bd98ffe114b4024bcfbe7380e9ae9cadc5"
-  version "1.0.0"
+  version "1.0.2"
 
   depends_on :macos
 
